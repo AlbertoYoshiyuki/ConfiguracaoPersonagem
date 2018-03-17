2a4a6594c6aeb264a434b9f04ab4a13375815f58
logonrm <logonrm@fiap.com.br>
2018-03-16 22:49:53 -0300

    Listener nos componentes

--- app/src/main/java/com/albertoyoshiyuki/configuracaopersonagem/PersonagemActivity.kt
+++ app/src/main/java/com/albertoyoshiyuki/configuracaopersonagem/PersonagemActivity.kt
@@ -2,11 +2,59 @@ package com.albertoyoshiyuki.configuracaopersonagem
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.View
+import android.widget.AdapterView
+import android.widget.SeekBar
+import kotlinx.android.synthetic.main.activity_personagem.*
 
 class PersonagemActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_personagem)
+
+        //Ao clicar e arrastar a barra tras a numeração na forma de texto
+        sbForca.setOnSeekBarChangeListener (object : SeekBar.OnSeekBarChangeListener{
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                tvForca.setText(progress.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
+        //Ao clicar no select altera a imagem
+        spClasses.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+                val tipoPersonagem = TipoPersonagemEnum.from(position)
+
+                when (tipoPersonagem) {
+                    TipoPersonagemEnum.ANAO -> trocarImagem(R.drawable.anao)
+                    TipoPersonagemEnum.ELFO -> trocarImagem(R.drawable.elfo)
+                    TipoPersonagemEnum.GUERREIRO -> trocarImagem(R.drawable.guerreiro)
+                    TipoPersonagemEnum.MAGO -> trocarImagem(R.drawable.mago)
+                    TipoPersonagemEnum.ORC -> trocarImagem(R.drawable.orc)
+                }
+
+            }
+        })
+
     }
+
+    fun trocarImagem(idImagem: Int) {
+        ivClasse.setImageDrawable(ContextCompat.getDrawable(this, idImagem))
+    }
+
 }
